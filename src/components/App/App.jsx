dd06b85fe7e5fb02824fbd669d348acd62ffa097
Dima Savchuk <118117711+DimaSavchuk@users.noreply.github.com>
2023-06-04 17:39:06 +0300

    import fix

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,9 @@
-import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import React from 'react';
 
-import AppRoutes from "../Routes/Routes";
-import Header from "../Header/Header";
-import Footer from "../Footer/Footer";
-import Sidebar from "../Sidebar/Sidebar";
+import AppRoutes from '../Routes/Routes';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+import Sidebar from '../Sidebar/Sidebar';
 
 const App = () => {
   return (
